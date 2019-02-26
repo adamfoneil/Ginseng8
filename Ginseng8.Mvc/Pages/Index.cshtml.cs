38456fc91163cd3a60b845596554f583e0ae35e2
adamo <adamosoftware@gmail.com>
2019-02-26 07:07:33 -0500

    Add project files.

---
+++ Ginseng8.Mvc/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ginseng8.Mvc.Pages
+{
+	public class IndexModel : PageModel
+	{
+		public void OnGet()
+		{
+
+		}
+	}
+}
