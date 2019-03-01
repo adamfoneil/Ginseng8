bbc6306a0e5a049d4a5a3380a4c63ab7fe2ce7f2
adamo <adamosoftware@gmail.com>
2019-03-01 16:19:31 -0500

    Setup/Labels #12 done

--- Ginseng8.Mvc/Pages/Setup/Labels.cshtml.cs
+++ Ginseng8.Mvc/Pages/Setup/Labels.cshtml.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Ginseng.Models;
+using Ginseng.Mvc.Queries;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Ginseng.Mvc.Pages.Setup
 {
@@ -8,8 +13,27 @@ namespace Ginseng.Mvc.Pages.Setup
 		{
 		}
 
-		public void OnGet()
+		public IEnumerable<Label> Labels { get; set; }
+
+		public void OnGet(bool isActive = true)
 		{
+			using (var cn = GetConnection())
+			{
+				Labels = new Labels() { OrgId = CurrentOrg.Id, IsActive = isActive }.Execute(cn);
+			}
+		}
+
+		public async Task<ActionResult> OnPostSave(Label label)
+		{
+			label.OrganizationId = CurrentOrg.Id;
+			await TrySaveAsync(label);
+			return RedirectToAction("/Setup/Labels");
+		}
+
+		public async Task<ActionResult> OnPostDelete(int id)
+		{
+			await TryDelete<Label>(id);
+			return RedirectToAction("/Setup/Labels");
 		}
 	}
 }
---
+++ Ginseng8.Mvc/Queries/Labels.cs
@@ -0,0 +1,15 @@
+﻿using Ginseng.Models;
+using Postulate.Base;
+
+namespace Ginseng.Mvc.Queries
+{
+	public class Labels : Query<Label>
+	{
+		public Labels() : base("SELECT * FROM [dbo].[Label] WHERE [OrganizationId]=@orgId AND [IsActive]=@isActive ORDER BY [Name]")
+		{
+		}
+
+		public int OrgId { get; set; }
+		public bool IsActive { get; set; }
+	}
+}
