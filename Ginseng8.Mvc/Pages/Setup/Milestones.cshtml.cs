ef5622d98f3760e944bc2013b7d248ccf2188577
adamo <adamosoftware@gmail.com>
2019-03-01 00:08:34 -0500

    more setup pages stubbed out

---
+++ Ginseng8.Mvc/Pages/Setup/Milestones.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Ginseng.Mvc.Pages.Setup
+{
+	public class MilestonesModel : AppPageModel
+	{
+		public MilestonesModel(IConfiguration config) : base(config)
+		{
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+}
---
+++ Ginseng8.Mvc/Pages/Setup/Projects.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Ginseng.Mvc.Pages.Setup
+{
+	public class ProjectsModel : AppPageModel
+	{
+		public ProjectsModel(IConfiguration config) : base(config)
+		{
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+}
