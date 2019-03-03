3a9ae6b8ecf54c8f891823d37165924cac05178f
adamo <adamosoftware@gmail.com>
2019-03-03 11:57:26 -0500

    added Test project, but stub method only

---
+++ Testing/QueryTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+	[TestClass]
+	public class QueryTests
+	{
+		[TestMethod]
+		public void WorkItemsQuery()
+		{
+			
+		}
+	}
+}
