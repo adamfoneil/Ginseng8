80bec6bdc3c46a27bc5832c4731081b3fdb277f1
adamo <adamosoftware@gmail.com>
2019-03-06 08:17:06 -0500

    started WorkItem/Edit

---
+++ Ginseng8.Mvc/Pages/WorkItem/Edit.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Ginseng.Mvc.Pages.WorkItem
+{
+	public class EditModel : AppPageModel
+	{
+		public EditModel(IConfiguration config) : base(config)
+		{
+		}
+
+		public Models.WorkItem WorkItem { get; set; }
+		
+		public async Task OnGetAsync(int id)
+		{
+			WorkItem = await Data.FindWhereAsync<Models.WorkItem>(new { OrganizationId = OrgId, Number = id });
+		}
+	}
+}
