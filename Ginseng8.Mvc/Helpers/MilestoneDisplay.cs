e0e82a130f4eb4340d7a05f83e82b331ad3b7892
Adam O'Neil <adamosoftware@gmail.com>
2019-03-08 14:43:54 -0500

    item fields changed to small buttons, cards implemented on My Items, started on sortable items

---
+++ Ginseng8.Mvc/Helpers/MilestoneDisplay.cs
@@ -0,0 +1,18 @@
+﻿using Ginseng.Mvc.Queries;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ginseng.Mvc.Helpers
+{
+	public static partial class HtmlHelpers
+	{
+		public static IHtmlContent MilestoneDisplay(this IHtmlHelper<dynamic> html, AllWorkItemsResult item)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Ginseng8.Mvc/wwwroot/js/Sortable.js
@@ -0,0 +1,7 @@
+﻿$(".item-sortable").sortable({
+    placeholder: "ui-state-highlight",
+    start: function (event, ui) {
+        // This event is triggered when sorting starts.
+        $('.ui-state-highlight').outerHeight($(ui.item).outerHeight()); // update height of placeholder to current item height
+    }
+});
