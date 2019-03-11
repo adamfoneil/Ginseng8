a005d12c12b5b86b78109deb11ad905bf3400978
Adam O'Neil <adamosoftware@gmail.com>
2019-03-11 18:24:58 -0400

    started on inline project body editing

---
+++ Ginseng8.Mvc/wwwroot/js/Projects.js
@@ -0,0 +1,8 @@
+﻿var viewProjectDetailLinks = document.querySelectorAll('.viewProjectBody');
+viewProjectDetailLinks.forEach(function (e) {
+    e.addEventListener('click', function (e) {
+        var projectId = e.target.getAttribute('data-project-id');
+        var viewDiv = document.getElementById('project-view-' + projectId);
+        $(viewDiv).slideToggle();
+    });
+});
