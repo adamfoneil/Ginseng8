3c186a1a93454aa6f01418a4e252563007b95338
Adam O'Neil <adamosoftware@gmail.com>
2019-03-10 20:13:12 -0400

    inline field updating

---
+++ Ginseng8.Mvc/Controllers/UpdateController.cs
@@ -0,0 +1,51 @@
+﻿using Ginseng.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace Ginseng.Mvc.Controllers
+{
+	[Authorize]
+	public class UpdateController : Controller
+	{
+		private readonly DataAccess _data;
+
+		public UpdateController(IConfiguration config)
+		{
+			_data = new DataAccess(config);			
+		}
+
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
+			_data.Initialize(User, TempData);
+		}
+
+		[HttpPost]
+		public async Task<JsonResult> WorkItem(WorkItem fields)
+		{
+			try
+			{
+				var workItem = await _data.FindWhereAsync<WorkItem>(new { OrganizationId = _data.CurrentOrg.Id, fields.Number });
+				workItem.ApplicationId = fields.ApplicationId;
+				workItem.ProjectId = fields.ProjectId;
+				workItem.MilestoneId = fields.MilestoneId;
+				workItem.SizeId = fields.SizeId;
+				workItem.CloseReasonId = fields.CloseReasonId;
+				workItem.ModifiedBy = User.Identity.Name;
+				workItem.DateModified = _data.CurrentUser.LocalTime;
+				await _data.TryUpdateAsync(workItem, 
+					r => r.ApplicationId, r => r.ProjectId, r => r.MilestoneId, r => r.SizeId, r => r.CloseReasonId,
+					r => r.ModifiedBy, r => r.DateModified);
+				return Json(new { success = true });
+			}
+			catch (Exception exc)
+			{
+				return Json(new { success = false, message = exc.Message });
+			}
+		}
+	}
+}
--- Ginseng8.Mvc/wwwroot/js/Dashboard.js
+++ Ginseng8.Mvc/wwwroot/js/Dashboard.js
@@ -1,68 +1,35 @@
-﻿function UpdateField(frm, url, divId, linkClass, callbackFunction) {
-    let formData = new FormData(frm);
-    fetch(url, {
-        method: 'post',
-        body: new URLSearchParams(formData)
-    }).then(function (response) {
-        response.text().then(function (text) {
-            var output = document.getElementById(divId);
-            output.innerHTML = text;
-            var links = output.querySelectorAll(linkClass);
-            // re-create the event handler on newly inserted content
-            links.forEach(function (e) {
-                e.addEventListener("click", callbackFunction);
+﻿var updateFields = document.querySelectorAll('.itemUpdate');
+updateFields.forEach(function (e) {
+    e.addEventListener("change", function (e) {
+        var frm = e.target.form;
+        var number = frm['Number'].value;
+        var loadingImg = document.getElementById('loading-' + number);
+        loadingImg.classList.add('visible');
+
+        var failImg = document.getElementById('update-failed-' + number);
+
+        try {
+            let formData = new FormData(frm);
+            fetch('/Update/WorkItem', {
+                method: 'post',
+                body: new URLSearchParams(formData)
+            }).then(function (response) {
+                return response.json();
+            }).then(function (result) {                
+                if (result.success) {
+                    var successImg = document.getElementById('update-success-' + number);
+                    $(successImg).show();
+                    $(successImg).fadeOut();
+                } else {
+                    $(failImg).show();
+                    failImg.setAttribute('title', result.message);
+                }
             });
-        });
+        } catch (e) {            
+            $(failImg).show();
+            failImg.setAttribute('title', result.message);
+        } finally {
+            $(loadingImg).hide();
+        }
     });
-}
-
-function OnSetMilestone(e) {
-    var link = e.target;
-    var number = link.getAttribute("data-number");
-    var msId = link.getAttribute("data-milestone-id");
-
-    var frm = document.getElementById("frmSetMilestone");
-    frm.number.value = number;
-    frm.milestoneId.value = msId;
-
-    UpdateField(frm, "/WorkItem/SetMilestone", "Milestone-" + number, ".setMilestone", OnSetMilestone);
-}
-
-var milestoneLinks = document.querySelectorAll(".setMilestone");
-milestoneLinks.forEach(function (e) {
-    e.addEventListener("click", OnSetMilestone);
-});
-
-function OnSetActivity(e) {
-    var link = e.target;
-    var number = link.getAttribute("data-number");
-    var activityId = link.getAttribute("data-activity-id");
-
-    var frm = document.getElementById("frmSetActivity");
-    frm.number.value = number;
-    frm.activityId.value = activityId;
-
-    UpdateField(frm, "/WorkItem/SetActivity", "Activity-" + number, ".setActivity", OnSetActivity);
-}
-
-var activityLinks = document.querySelectorAll(".setActivity");
-activityLinks.forEach(function (e) {
-    e.addEventListener("click", OnSetActivity);
-});
-
-function OnSetEstimate(e) {
-    var link = e.target;
-    var number = link.getAttribute("data-number");
-    var sizeId = link.getAttribute("data-size-id");
-
-    var frm = document.getElementById("frmSetEstimate");
-    frm.number.value = number;
-    frm.sizeId.value = sizeId;
-
-    UpdateField(frm, "/WorkItem/SetEstimate", "Estimate-" + number, ".setEstimate", OnSetEstimate);
-}
-
-var estimateLinks = document.querySelectorAll(".setEstimate");
-estimateLinks.forEach(function (e) {
-    e.addEventListener("click", OnSetEstimate);
 });
