c6df9220b0ee520809dd35958f0467d14b2d9d73
Adam O'Neil <adamosoftware@gmail.com>
2019-03-16 16:49:35 -0400

    started on search, but not sure what I want

---
+++ Ginseng8.Mvc/Pages/Search.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Ginseng.Mvc.Pages
+{
+	public class SearchModel : AppPageModel
+	{
+		public SearchModel(IConfiguration config) : base(config)
+		{
+		}
+
+		public async Task OnGetAsync(string query)
+		{
+		}
+	}
+}
---
+++ Ginseng8.Mvc/Queries/Search/WorkItemTitles.cs
@@ -0,0 +1,25 @@
+﻿using Ginseng.Models;
+using Postulate.Base;
+
+namespace Ginseng.Mvc.Queries.Search
+{
+	public class WorkItemTitles : Query<WorkItem>
+	{
+		public WorkItemTitles() : base(
+			@"SELECT TOP (10)
+				[wi].[Number],
+				[wi].[Title],
+				[wi].[DateCreated],
+			FROM 
+				[dbo].[WorkItem] [wi]
+			WHERE
+				[OrganizationId]=@orgId AND 
+				[Title] LIKE '%'+@title+'%'
+			ORDER BY
+				[Number]")
+		{
+		}
+
+		public int OrgId { get; set; }
+	}
+}
