c95e5e7769264de7805e31262ea988876377219a
Adam O'Neil <adamosoftware@gmail.com>
2019-03-18 12:41:06 -0400

    removed WorkItemPriority UserId FK so we can prioritize backlog (i.e. UserId = 0)

--- Ginseng8.Models/WorkItemPriority.cs
+++ Ginseng8.Models/WorkItemPriority.cs
@@ -3,13 +3,13 @@ using Postulate.Base.Attributes;
 
 namespace Ginseng.Models
 {	
+	[TrackChanges(IgnoreProperties = "DateModified,ModifiedBy")]
 	public class WorkItemPriority : BaseTable
 	{
 		[References(typeof(WorkItem))]
 		[PrimaryKey]
 		public int WorkItemId { get; set; }
-
-		[References(typeof(UserProfile))]
+		
 		public int UserId { get; set; }
 
 		public int Value { get; set; }
