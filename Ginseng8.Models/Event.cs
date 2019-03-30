71297a68ec265dbd3a9de32b54bb16e02afa1c90
Adam O'Neil <adamosoftware@gmail.com>
2019-03-30 17:47:35 -0400

    event seed data

--- Ginseng8.Models/Event.cs
+++ Ginseng8.Models/Event.cs
@@ -1,6 +1,9 @@
 ﻿using Ginseng.Models.Conventions;
 using Postulate.Base.Attributes;
+using Postulate.Base.Extensions;
+using Postulate.SqlServer;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
 
 namespace Ginseng.Models
 {
@@ -9,5 +12,24 @@ namespace Ginseng.Models
 		[MaxLength(50)]
 		[PrimaryKey]
 		public string Name { get; set; }
-	}
+
+		public static DataTable GetSeedData()
+		{
+			return new Event[]
+			{
+				new Event() { Name = "Work Item Created" },
+				new Event() { Name = "Work Item Closed" },
+				new Event() { Name = "Milestone Changed" },
+				new Event() { Name = "Project Changed" },
+				new Event() { Name = "Comment Added" },
+				new Event() { Name = "Impediment Added" },
+				new Event() { Name = "Priority Changed" },
+				new Event() { Name = "Hand Off" },
+				new Event() { Name = "Work Item Field Changed" },
+				new Event() { Name = "Milestone Date Changed" },
+				new Event() { Name = "Estimate Changed" },
+				new Event() { Name = "Project Added" }
+			}.ToDataTable(new SqlServerIntegrator(), excludeIdentity: true);
+		}
+	}	
 }
