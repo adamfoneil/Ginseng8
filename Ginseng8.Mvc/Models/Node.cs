7139309eee6e06d6835fb4e7ffb41506b7f1f162
Adam O'Neil <adamosoftware@gmail.com>
2019-04-01 12:54:56 -0400

    working on node resolution from path list

---
+++ Ginseng8.Mvc/Models/Node.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ginseng.Mvc.Models
+{
+	public class Node<T>
+	{
+		public string Name { get; set; }
+		public IEnumerable<Node<T>> Children { get; set; }
+		public IEnumerable<T> Items { get; set; }
+
+		public static Node<T> ResolveStructure(IEnumerable<T> items, Func<T, string> getPath, char separator)
+		{
+			Node<T> result = new Node<T>() { Name = "Root" };
+
+			IEnumerable<string> parsePath(T item)
+			{
+				return getPath.Invoke(item).Split(new char[] { separator }, StringSplitOptions.RemoveEmptyEntries).Select(s => s.ToString());
+			};
+
+			var navs = items.Select(i => new
+			{
+				Path = string.Join(separator, parsePath(i)), // make sure path has no spaces or repeating separators
+				Folders = parsePath(i).ToArray(), // split the folder elements into array
+				Item = i
+			});
+			
+			int maxLevel = navs.Max(i => i.Folders.Length);
+
+			for (int level = 0; level < maxLevel - 1; level++)
+			{
+				List<Node<T>> children = new List<Node<T>>();
+				
+				var uniqueFolders = navs.GroupBy(nav => string.Join(separator, nav.Folders.Take(level + 1)));
+				foreach (var folderGrp in uniqueFolders)
+				{					
+					Node<T> child = new Node<T>() { Name = folderGrp.Key.Split(separator).Last() };					
+
+					child.Items = folderGrp
+						.Where(nav => nav.Path.Equals(folderGrp.Key))
+						.Select(nav => nav.Item);
+
+					children.Add(child);
+					result.Children = children;
+				}
+			}
+
+			return result;
+		}
+	}	
+}
---
+++ Testing/NodeTest.cs
@@ -0,0 +1,41 @@
+﻿using Ginseng.Mvc.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testing
+{
+	[TestClass]
+	public class NodeTest
+	{
+		[TestMethod]
+		public void TestNodeResolution()
+		{
+			var items = new SampleItem[]
+			{
+				new SampleItem() { Path = "Home", Name = "This" },
+				new SampleItem() { Path = "Home", Name = "That"},
+				new SampleItem() { Path = "Pushkin", Name = "Fistula" },
+				new SampleItem() { Path = "Pushkin/Level1", Name = "Hepsulor" },
+				new SampleItem() { Path = "Home", Name = "Other" },
+				new SampleItem() { Path = "Away", Name = "Jenga" },
+				new SampleItem() { Path = "Home/Level1", Name = "Hello" },
+				new SampleItem() { Path = "Home/Level1", Name = "Goodbye" },
+				new SampleItem() { Path = "Home/Level2", Name = "Whatever" },
+				new SampleItem() { Path = "Home/Level2", Name = "Threnksadoodle" },
+				new SampleItem() { Path = "Home/Level3/Aquatics", Name = "Wakwa" },
+				new SampleItem() { Path = "Home/Level3/Aquatics", Name = "Yimyim" },
+				new SampleItem() { Path = "Away", Name = "Plopsie" }
+			};
+
+			var node = Node<SampleItem>.ResolveStructure(items, (i) => i.Path, '/');
+		}
+	}
+
+	public class SampleItem
+	{
+		public string Path { get; set; }
+		public string Name { get; set; }
+	}
+}
