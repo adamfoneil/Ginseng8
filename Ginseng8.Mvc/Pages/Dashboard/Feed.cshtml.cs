aa79a60162992d9fb878bbd622735835d63ecc0e
Adam O'Neil <adamosoftware@gmail.com>
2019-04-05 08:47:00 -0400

    #1159 generate default event subscriptions

--- Ginseng8.Mvc/Pages/Dashboard/Feed.cshtml.cs
+++ Ginseng8.Mvc/Pages/Dashboard/Feed.cshtml.cs
@@ -1,8 +1,11 @@
-﻿using Ginseng.Models;
+﻿using Dapper;
+using Ginseng.Models;
 using Ginseng.Mvc.Queries;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -24,11 +27,35 @@ namespace Ginseng.Mvc.Pages.Work
 			using (var cn = Data.GetConnection())
 			{
 				Events = await new Events().ExecuteAsync(cn);
+
+				await CreateDefaultEventSubscriptionsAsync(cn);
+
 				var subs = await new MyEventSubscriptions() { OrgId = OrgId, UserId = UserId, AppId = CurrentOrgUser.CurrentAppId ?? 0 }.ExecuteAsync(cn);
 				Subscriptions = subs.ToDictionary(row => row.EventId);
 				var eventIds = subs.Where(s => s.Visible).Select(es => es.EventId).ToArray();
 				EventLogs = await new EventLogs() { EventIds = eventIds, OrgId = OrgId, AppId = CurrentOrgUser.CurrentAppId ?? 0 }.ExecuteAsync(cn);
 			}
+		}
+
+		private async Task CreateDefaultEventSubscriptionsAsync(SqlConnection cn)
+		{
+			await cn.ExecuteAsync(
+				@"INSERT INTO [dbo].[EventSubscription] (
+					[EventId], [OrganizationId], [ApplicationId], [UserId], [Visible], [SendEmail], [SendText], [InApp],
+					[CreatedBy], [DateCreated]
+				) SELECT
+					[e].[Id], @orgId, @appId, @userId, 1, 1, 1, 1, @createdBy, @dateCreated
+				FROM 
+					[app].[Event] [e]
+				WHERE
+					NOT EXISTS(
+						SELECT 1 FROM [dbo].[EventSubscription] 
+						WHERE 
+							[EventId]=[e].[Id] AND
+							[OrganizationId]=@orgId AND
+							[ApplicationId]=@appId AND
+							[UserId]=@userId
+					)", new { OrgId, appId = CurrentOrgUser.CurrentAppId, UserId, CreatedBy = User.Identity.Name, dateCreated = CurrentUser.LocalTime });
 		}
 	}
 }
--- Ginseng8.Mvc/Queries/MyEventSubscriptions.cs
+++ Ginseng8.Mvc/Queries/MyEventSubscriptions.cs
@@ -6,38 +6,14 @@ namespace Ginseng.Mvc.Queries
 	public class MyEventSubscriptions : Query<EventSubscription>
 	{
 		public MyEventSubscriptions() : base(
-			@"WITH [source] AS (
-				SELECT 
-					[es].*
-				FROM 
-					[dbo].[EventSubscription] [es]
-				WHERE
-					[UserId]=@userId AND
-					[OrganizationId]=@orgId AND
-					[ApplicationId]=@appId
-			) SELECT
-				[source].*
-			FROM
-				[source]
-			UNION
-			SELECT 
-				0 AS [Id],
-				[ev].[Id] AS [EventId],
-				@orgId AS [OrganizationId],
-				@appId AS [AppliationId],
-				@userId AS [UserId],
-				1 AS [Visible],
-				0 AS [SendEmail],
-				0 AS [SendText],
-				0 AS [InApp],
-				NULL AS [CreatedBy],
-				NULL AS [DateCreated],
-				NULL AS [ModifiedBy],
-				NULL AS [DateModified]
-			FROM
-				[app].[Event] [ev]
+			@"SELECT 
+				[es].*
+			FROM 
+				[dbo].[EventSubscription] [es]
 			WHERE
-				NOT EXISTS(SELECT 1 FROM [source] WHERE [EventId]=[ev].[Id])")
+				[UserId]=@userId AND
+				[OrganizationId]=@orgId AND
+				[ApplicationId]=@appId")
 		{
 		}
 
