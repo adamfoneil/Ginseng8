438c2be2e1440adfb3f61b1141c2d7f6a666d297
Adam O'Neil <adamosoftware@gmail.com>
2019-04-07 09:38:54 -0400

    started on help content

---
+++ Ginseng8.Mvc/Pages/Help/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Ginseng.Mvc.Pages.Help
+{
+	public class IndexModel : AppPageModel
+	{
+		public IndexModel(IConfiguration config) : base(config)
+		{
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+}
