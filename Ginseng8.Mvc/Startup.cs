0a03acca0a8ad79768e6770c4fb64eb6fa667457
Vadim <vadim.a@outlook.com>
2019-04-10 20:37:18 +0300

    identity email sending feature

--- Ginseng8.Mvc/Startup.cs
+++ Ginseng8.Mvc/Startup.cs
@@ -1,9 +1,11 @@
 using Ginseng.Mvc.Data;
+using Ginseng.Mvc.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -34,7 +36,7 @@ namespace Ginseng.Mvc
 				options.UseSqlServer(
 					Configuration.GetSection("ConnectionStrings").GetValue<string>("Default")
 					/*Configuration.GetConnectionString("DefaultConnection")*/));
-			services.AddDefaultIdentity<IdentityUser>()
+			services.AddDefaultIdentity<IdentityUser>(ConfigureIdentity)
 				.AddDefaultUI(UIFramework.Bootstrap4)
 				.AddEntityFrameworkStores<ApplicationDbContext>();
 
@@ -45,10 +47,12 @@ namespace Ginseng.Mvc
 					options.ClientSecret = Configuration.GetSection("Google").GetValue<string>("ClientSecret");
 				});
 
+            services.AddTransient<IEmailSender, Email>();
+
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -74,5 +78,10 @@ namespace Ginseng.Mvc
 				routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
 			});
 		}
-	}
+
+        private void ConfigureIdentity(IdentityOptions options)
+        {
+            options.SignIn.RequireConfirmedEmail = true;
+        }
+    }
 }
