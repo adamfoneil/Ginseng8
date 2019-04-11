995ec26dcbaf07389a1a67b5f41870c7424351dc
Adam O'Neil <adamosoftware@gmail.com>
2019-04-11 08:46:25 -0400

    #1209 this is better, but not toggling in database

--- Ginseng8.Mvc/wwwroot/js/Notification.js
+++ Ginseng8.Mvc/wwwroot/js/Notification.js
@@ -1,23 +1,22 @@
 ﻿var notifyToggles = document.querySelectorAll('.toggleNotification');
-notifyToggles.forEach(function (ele) {
-    ele.addEventListener('click', function (ev) {
-        var anchor = $(ev.target).parent('a')[0];
-        var id = anchor.getAttribute('data-id');
 
-        var data = {
-            id: id,
-            propertyName: anchor.getAttribute('data-property-name'),
-            tableName: anchor.getAttribute('data-table-name')
-        };
+$('.toggleNotification').on('click', function () { 
+        
+    var id = $(this).attr('data-id');
 
-        var formData = getFormData(data);
-        fetch('/Update/ToggleNotification', {
-            method: 'post',
-            body: formData
-        }).then(function (response) {
-            return response.text();
-        }).then(function (result) {
-            $('#notifyOptions-' + id).html(result);
-        });
-    });
+    var data = {
+        id: id,
+        propertyName: $(this).attr('data-property-name'),
+        tableName: $(this).attr('data-table-name')
+    };
+
+    var formData = getFormData(data);
+    fetch('/Update/ToggleNotification', {
+        method: 'post',
+        body: formData
+    }).then(function (response) {
+        return response.text();
+    }).then(function (result) {
+        $('#notifyOptions-' + id).html(result);
+    });    
 });
