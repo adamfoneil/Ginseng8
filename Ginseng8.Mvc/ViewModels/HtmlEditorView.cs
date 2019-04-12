0c2035ecd01449b7de80f1ed6f4499380349a939
Adam O'Neil <adamosoftware@gmail.com>
2019-04-12 07:39:56 -0400

    added HtmlEditorView.OverrideId and UploadId

--- Ginseng8.Mvc/ViewModels/HtmlEditorView.cs
+++ Ginseng8.Mvc/ViewModels/HtmlEditorView.cs
@@ -11,5 +11,20 @@ namespace Ginseng.Mvc.ViewModels
 		public bool AllowComments { get; set; }
 		public string UploadFolderName { get; set; }
 		public Dictionary<string, object> PostFields { get; set; }
+
+		/// <summary>
+		/// Used to inject work item numbers for upload storage path purposes.
+		/// Work items are odd in that they have both an Id and Number. The Number is what the user sees/expects,
+		/// but the Id ensures editor compatibility in different parts of app.
+		/// </summary>
+		public int OverrideId { get; set; }
+
+		/// <summary>
+		/// This should be used to determine the id of uploaded content
+		/// </summary>
+		public int UploadId
+		{
+			get { return (OverrideId != 0) ? OverrideId : Id; }
+		}
 	}
 }
