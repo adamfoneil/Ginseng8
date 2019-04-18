867fc18785c10c136e9282bea7d72b1f20ac1b09
Adam O'Neil <adamosoftware@gmail.com>
2019-04-18 13:54:04 -0400

    created ExternalRegistration

---
+++ Ginseng8.Models/ExternalRegistration.cs
@@ -0,0 +1,31 @@
+﻿using Postulate.Base.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ginseng.Models
+{
+	/// <summary>
+	/// Created by <see cref="OrganizationUser.ConnectPrincipalAsync(System.Data.SqlClient.SqlConnection, System.Security.Claims.ClaimsPrincipal, string)"/>
+	/// Then an insert trigger on dbo.AspNetUsers generates dbo.OrganizationUser from this
+	/// </summary>
+	[Identity(nameof(Id))]
+	public class ExternalRegistration
+	{
+		public int Id { get; set; }
+
+		[MaxLength(255)]
+		[Required]
+		[PrimaryKey]
+		public string UserName { get; set; }
+
+		[MaxLength(50)]
+		[Required]
+		public string DisplayName { get; set; }
+
+		[MaxLength(50)]
+		[Required]
+		public string TenantName { get; set; }
+
+		public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+	}
+}
