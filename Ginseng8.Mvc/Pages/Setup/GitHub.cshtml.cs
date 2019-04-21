ce43fc48492eb851bd4e55a037c87a7e259c177a
Adam O'Neil <adamosoftware@gmail.com>
2019-04-20 20:48:12 -0400

    started on this

---
+++ Ginseng8.Mvc/Pages/Setup/GitHub.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace Ginseng.Mvc.Pages.Setup
+{
+    public class GitHubModel : AppPageModel
+    {
+		public GitHubModel(IConfiguration config) : base(config)
+		{
+		}
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
