845e63f5c2e6eb37cf41587c51c951a2ed3b8ad8
Adam O'Neil <adamosoftware@gmail.com>
2019-04-30 12:58:51 -0400

    started on Freshdesk.Client project

---
+++ Freshdesk.Client/FreshdeskClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Freshdesk.Client
+{
+	public class FreshdeskClient
+	{
+		private readonly string _apiKey;
+
+		public FreshdeskClient(string apiKey)
+		{
+			_apiKey = apiKey;
+		}
+
+
+	}
+}
