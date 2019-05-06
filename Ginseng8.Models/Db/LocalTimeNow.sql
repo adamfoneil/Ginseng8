404a1ced9518801e36960d570b4ebda1eab679fa
Adam O'Neil <adamosoftware@gmail.com>
2019-05-06 11:35:41 -0400

    added local time functions

---
+++ Ginseng8.Models/Db/LocalTimeNow.sql
@@ -0,0 +1,8 @@
+﻿CREATE FUNCTION [dbo].[LocalTimeNow](
+	@userName nvarchar(50)
+) RETURNS datetime AS
+BEGIN
+	DECLARE @result datetime
+	SET @result = dbo.LocalTime(@userName, getutcdate())
+	RETURN @result
+END
