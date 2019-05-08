7127101033aff5bf45977e08648a053dd52939a9
Adam O'Neil <adamosoftware@gmail.com>
2019-05-08 11:15:20 -0400

    started on Invoice/View, but need to add Organization.InvoiceHeader

---
+++ Ginseng8.Mvc/Pages/Invoice/View.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Ginseng.Models;
+using Ginseng.Mvc.Queries;
+using Ginseng.Mvc.Queries.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Postulate.SqlServer.IntKey;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ginseng.Mvc.Pages.Invoice
+{
+    [Authorize]
+    public class ViewModel : AppPageModel
+    {
+        public ViewModel(IConfiguration config) : base(config)
+        {
+        }
+
+        public Ginseng.Models.Invoice Header { get; set; }
+        public IEnumerable<WorkLogsResult> Details { get; set; }
+
+        public async Task OnGetAsync(int id)
+        {
+            using (var cn = Data.GetConnection())
+            {
+                Header = await cn.FindAsync<Ginseng.Models.Invoice>(id);
+                Details = await new AllInvoiceWorkLogs() { OrgId = OrgId, InvoiceId = id }.ExecuteAsync(cn);
+            }
+        }
+    }
+}
