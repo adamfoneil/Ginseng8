48531a6cc8516da2f5cb37301c6b41808fbab531
Adam O'Neil <adamosoftware@gmail.com>
2019-05-08 15:26:47 -0400

    some layout tweaks to invoice page

--- Ginseng8.Mvc/Pages/Invoice/View.cshtml.cs
+++ Ginseng8.Mvc/Pages/Invoice/View.cshtml.cs
@@ -1,6 +1,5 @@
 ﻿using Ginseng.Mvc.Queries;
 using Ginseng.Mvc.Queries.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
 using Postulate.SqlServer.IntKey;
 using System.Collections.Generic;
@@ -9,7 +8,6 @@ using System.Threading.Tasks;
 
 namespace Ginseng.Mvc.Pages.Invoice
 {
-    [Authorize]
     public class ViewModel : AppPageModel
     {
         public ViewModel(IConfiguration config) : base(config)
@@ -20,12 +18,12 @@ namespace Ginseng.Mvc.Pages.Invoice
         public IEnumerable<WorkLogsResult> Details { get; set; }
         public IEnumerable<CalendarWeeksResult> Weeks { get; set; }
 
-        public async Task OnGetAsync(int id)
+        public async Task OnGetAsync(int id, int orgId)
         {
             using (var cn = Data.GetConnection())
             {
-                Header = await cn.FindWhereAsync<Ginseng.Models.Invoice>(new { OrganizationId = OrgId, Number = id });
-                Details = await new AllInvoiceWorkLogs() { OrgId = OrgId, InvoiceId = Header.Id }.ExecuteAsync(cn);
+                Header = await cn.FindWhereAsync<Ginseng.Models.Invoice>(new { OrganizationId = orgId, Number = id });
+                Details = await new AllInvoiceWorkLogs() { OrgId = orgId, InvoiceId = Header.Id }.ExecuteAsync(cn);
                 if (Details.Any())
                 {
                     Weeks = await new CalendarWeeks()
