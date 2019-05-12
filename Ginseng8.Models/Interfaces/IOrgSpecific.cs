fa3e795407cafe2277ca38cf369be7df5e15cdf8
adamo <adamosoftware@gmail.com>
2019-05-12 16:06:11 -0400

    removed obsolete interface IFeedItem, and updated comment on IOrgSpecific

--- Ginseng8.Models/Interfaces/IOrgSpecific.cs
+++ Ginseng8.Models/Interfaces/IOrgSpecific.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Text;
+﻿using System.Data;
 using System.Threading.Tasks;
 
 namespace Ginseng.Models.Interfaces
 {
     /// <summary>
     /// Implement this model classes to provide a standard way of accessing a record's containing OrganizationId.
-    /// This is how tenant isolation is checked by 
+    /// This is how tenant isolation is checked in <see cref="Models.Conventions.BaseTable.CheckOrgPermissionAsync(IDbConnection, Postulate.Base.Interfaces.IUser)"/>
     /// </summary>
     public interface IOrgSpecific
     {
