097af0c3efd9fa3d195ae7733e2ef1c92c3cdbbd
adamo <adamosoftware@gmail.com>
2019-05-14 15:29:13 -0400

    code cleanup

--- Testing/QueryTests.cs
+++ Testing/QueryTests.cs
@@ -7,97 +7,97 @@ using System.Data.SqlClient;
 
 namespace Testing
 {
-	[TestClass]
-	public class QueryTests
-	{
-		// helpful sources:
-		// https://www.jerriepelser.com/blog/using-configuration-files-in-dotnet-core-unit-tests/
-		// https://weblog.west-wind.com/posts/2018/Feb/18/Accessing-Configuration-in-NET-Core-Test-Projects
+    [TestClass]
+    public class QueryTests
+    {
+        // helpful sources:
+        // https://www.jerriepelser.com/blog/using-configuration-files-in-dotnet-core-unit-tests/
+        // https://weblog.west-wind.com/posts/2018/Feb/18/Accessing-Configuration-in-NET-Core-Test-Projects
 
-		private IConfiguration _config;
+        private IConfiguration _config;
 
-		public QueryTests()
-		{
-			_config = new ConfigurationBuilder().AddJsonFile("config.json").Build();
-		}
+        public QueryTests()
+        {
+            _config = new ConfigurationBuilder().AddJsonFile("config.json").Build();
+        }
 
-		private SqlConnection GetConnection()
-		{
-			string connectionStr = _config.GetSection("ConnectionStrings").GetValue<string>("Default");
-			return new SqlConnection(connectionStr);
-		}
+        private SqlConnection GetConnection()
+        {
+            string connectionStr = _config.GetSection("ConnectionStrings").GetValue<string>("Default");
+            return new SqlConnection(connectionStr);
+        }
 
-		private void TestQuery<TQuery>() where TQuery : ITestableQuery, new()
-		{
-			var qry = new TQuery();
-			using (var cn = GetConnection())
-			{
-				foreach (var testCase in qry.GetTestCases())
-				{
-					testCase.TestExecute(cn);
-				}
-			}
-		}
+        private void TestQuery<TQuery>() where TQuery : ITestableQuery, new()
+        {
+            var qry = new TQuery();
+            using (var cn = GetConnection())
+            {
+                foreach (var testCase in qry.GetTestCases())
+                {
+                    testCase.TestExecute(cn);
+                }
+            }
+        }
 
-		[TestMethod]
-		public void WorkItemsQuery()
-		{
-			TestQuery<OpenWorkItems>();			
-		}
+        [TestMethod]
+        public void WorkItemsQuery()
+        {
+            TestQuery<OpenWorkItems>();
+        }
 
-		[TestMethod]
-		public void EventLogsQuery()
-		{
-			TestQuery<EventLogs>();
-		}
+        [TestMethod]
+        public void EventLogsQuery()
+        {
+            TestQuery<EventLogs>();
+        }
 
-		[TestMethod]
-		public void EventSubscriptionEmailNotifications()
-		{
-			TestQuery<InsertEventSubscriptionEmailNotifications>();
-		}
+        [TestMethod]
+        public void EventSubscriptionEmailNotifications()
+        {
+            TestQuery<InsertEventSubscriptionEmailNotifications>();
+        }
 
-		[TestMethod]
-		public void EventSubscriptionTextNotifications()
-		{
-			TestQuery<InsertEventSubscriptionTextNotifications>();
-		}
+        [TestMethod]
+        public void EventSubscriptionTextNotifications()
+        {
+            TestQuery<InsertEventSubscriptionTextNotifications>();
+        }
 
-		[TestMethod]
-		public void ActivitySubscriptionEmailNotifications()
-		{
-			TestQuery<InsertActivitySubscriptionEmailNotifications>();
-		}
+        [TestMethod]
+        public void ActivitySubscriptionEmailNotifications()
+        {
+            TestQuery<InsertActivitySubscriptionEmailNotifications>();
+        }
 
-		[TestMethod]
-		public void ActivitySubscriptionTextNotifications()
-		{
-			TestQuery<InsertActivitySubscriptionTextNotifications>();
-		}
+        [TestMethod]
+        public void ActivitySubscriptionTextNotifications()
+        {
+            TestQuery<InsertActivitySubscriptionTextNotifications>();
+        }
 
-		[TestMethod]
-		public void ProjectInfoQuery()
-		{
-			TestQuery<ProjectInfo>();
-		}
+        [TestMethod]
+        public void ProjectInfoQuery()
+        {
+            TestQuery<ProjectInfo>();
+        }
 
-		[TestMethod]
-		public void PendingNotificationsQuery()
-		{
-			TestQuery<PendingNotifications>();
-		}
+        [TestMethod]
+        public void PendingNotificationsQuery()
+        {
+            TestQuery<PendingNotifications>();
+        }
 
-		[TestMethod]
-		public void OrgUserByNameQuery()
-		{
-			TestQuery<OrgUserByName>();
-		}
+        [TestMethod]
+        public void OrgUserByNameQuery()
+        {
+            TestQuery<OrgUserByName>();
+        }
 
-		[TestMethod]
-		public void NextPriorityQuery()
-		{
-			TestQuery<NextPriority>();
-		}
+        [TestMethod]
+        public void NextPriorityQuery()
+        {
+            TestQuery<NextPriority>();
+        }
 
         [TestMethod]
         public void AllPendingWorkLogsQuery()
@@ -122,5 +122,5 @@ namespace Testing
         {
             TestQuery<Labels>();
         }
-	}
+    }
 }
