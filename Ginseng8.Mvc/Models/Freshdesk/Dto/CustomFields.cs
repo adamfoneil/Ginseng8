5a6242b495c4366ed9f751c21e7929618c55067d
adamo <adamosoftware@gmail.com>
2019-05-15 15:36:17 -0400

    fixed field name, thanks to Vadim's advice

--- Ginseng8.Mvc/Models/Freshdesk/Dto/CustomFields.cs
+++ Ginseng8.Mvc/Models/Freshdesk/Dto/CustomFields.cs
@@ -4,7 +4,7 @@ namespace Ginseng.Mvc.Models.Freshdesk.Dto
 {
     public class CustomFields
     {
-        [JsonProperty("cf_ginsengworkitem")]
+        [JsonProperty("cf_work_item")]
         public string GinsengWorkItem { get; set; }
     }
 }
