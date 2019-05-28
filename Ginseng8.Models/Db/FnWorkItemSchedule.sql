a8fb491a9c9c884052dad40aabab917dfc7118fa
adamo <adamosoftware@gmail.com>
2019-05-28 15:29:01 -0400

    #1317 added priority column

--- Ginseng8.Models/Db/FnWorkItemSchedule.sql
+++ Ginseng8.Models/Db/FnWorkItemSchedule.sql
@@ -3,7 +3,9 @@ ALTER FUNCTION [dbo].[FnWorkItemSchedule](
 	@userId int
 ) RETURNS @results TABLE (
 	[Number] int NOT NULL,
-	[Date] date NOT NULL
+	[Date] date NOT NULL,
+	[Hours] int NOT NULL,
+	[Priority] int NOT NULL
 ) AS
 BEGIN
 
@@ -24,28 +26,29 @@ BEGIN
 	DECLARE @openItems TABLE (
 		[Number] int NOT NULL,
 		[EstimateHours] int NOT NULL,
-		[Hour] int
+		[Hour] int NOT NULL,
+		[Priority] int NOT NULL
 	);
 
 	INSERT INTO @openItems (
-		[Number], [EstimateHours], [Hour]
+		[Number], [EstimateHours], [Hour], [Priority]
 	) SELECT
-		[Number], [EstimateHours], ROW_NUMBER() OVER (ORDER BY [Priority]) AS [Hour]
+		[Number], [EstimateHours], ROW_NUMBER() OVER (ORDER BY [Priority]) AS [Hour], [Priority]
 	FROM
 		[dbo].[FnOpenItemEstimateRanges](@orgId, @userId) [oie]
 		CROSS APPLY [dbo].[FnIntRange]([oie].[StartHours], [oie].[EndHours])
 
 	INSERT INTO @results (
-		[Number], [Date]
+		[Number], [Date], [Hours], [Priority]
 	) SELECT
-		[oi].[Number], [wh].[Date]
+		[oi].[Number], [wh].[Date], COUNT(1), [Priority]
 	FROM
 		@workHours [wh] INNER JOIN @openItems [oi] ON [wh].[Hour]=[oi].[Hour]
 	GROUP BY
-		[oi].[Number], [wh].[Date]
+		[oi].[Number], [wh].[Date], [oi].[Priority]
 
 	RETURN
 END
 GO
 
-SELECT * FROM [dbo].[FnWorkItemSchedule](1, 10) ORDER BY [Date]
+SELECT * FROM [dbo].[FnWorkItemSchedule](1, 10) ORDER BY [Date], [Priority]
