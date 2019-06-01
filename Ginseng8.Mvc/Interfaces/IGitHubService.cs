b49c1db53f88b80f30454ee383010d4c921f1288
adamo <adamosoftware@gmail.com>
2019-06-01 11:29:41 -0400

    some notes for Vadim

--- Ginseng8.Mvc/Interfaces/IGitHubService.cs
+++ Ginseng8.Mvc/Interfaces/IGitHubService.cs
@@ -12,17 +12,22 @@ namespace Ginseng.Mvc.Interfaces
 
         /// <summary>
         /// Should include public and private
+        /// Vadim, please focus here
         /// </summary>
         Task<IEnumerable<Repository>> ListRepositoriesAsync();
 
         /// <summary>
         /// List branches of the given repo Id
+        /// Vadim, please focus on this as well
         /// </summary>
         Task<IEnumerable<Branch>> ListBranchesAsync(int repositoryId);
 
         /// <summary>
         /// Associates commit messages with work items based on the branch and message formatting
+        /// Vadim, don't worry about this one -- AO will implement
         /// </summary>
         Task OnWebhookPushAsync(PushEventPayload @event);
+
+        // webhook for pull requests?
     }
 }
