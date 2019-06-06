9db563b7649cdd45ae05f514a9d47ec3c3e69bcf
Adam O'Neil <adamosoftware@gmail.com>
2019-06-06 14:56:35 -0400

    added NewItemAppLabel table

---
+++ Ginseng8.Models/NewItemAppLabel.cs
@@ -0,0 +1,19 @@
+﻿using Ginseng.Models.Conventions;
+using Postulate.Base.Attributes;
+
+namespace Ginseng.Models
+{
+    /// <summary>
+    /// Associates a label with an app for use with Dashboard/New
+    /// </summary>
+    public class NewItemAppLabel : BaseTable
+    {
+        [References(typeof(Application))]
+        [PrimaryKey]
+        public int ApplicationId { get; set; }
+
+        [References(typeof(Label))]
+        [PrimaryKey]
+        public int LabelId { get; set; }
+    }
+}
