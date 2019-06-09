8f81bb8c129883f6cedd7021c4dd276ee12f87f4
adamo <adamosoftware@gmail.com>
2019-06-09 10:19:13 -0400

    added DeveloperMilestone table and code cleanup on Milestone.cs

---
+++ Ginseng8.Models/DeveloperMilestone.cs
@@ -0,0 +1,22 @@
+﻿using Ginseng.Models.Conventions;
+using Postulate.Base.Attributes;
+using System;
+
+namespace Ginseng.Models
+{
+    /// <summary>
+    /// Associates a developer with a milestone in order to determine her/his available hours and feasibility of work items
+    /// </summary>
+    public class DeveloperMilestone : BaseTable
+    {
+        [References(typeof(UserProfile))]
+        [PrimaryKey]
+        public int DeveloperId { get; set; }
+
+        [References(typeof(Milestone))]
+        [PrimaryKey]
+        public int MilestoneId { get; set; }
+
+        public DateTime StartDate { get; set; }
+    }
+}
