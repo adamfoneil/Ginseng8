ee5a708f9a4741f903c6f91f9d6ca394fdd8922f
Adam O'Neil <adamosoftware@gmail.com>
2019-06-19 11:02:32 -0400

    use auto property refactor

--- Ginseng8.Mvc/Pages/Dashboard/Hung.cshtml.cs
+++ Ginseng8.Mvc/Pages/Dashboard/Hung.cshtml.cs
@@ -10,37 +10,32 @@ namespace Ginseng.Mvc.Pages.Work
     [Authorize]
     public class IndexModel : DashboardPageModel
     {
-        private IEnumerable<OpenWorkItemsResult> _workItems;
-
         public IndexModel(IConfiguration config) : base(config)
         {
             ShowLabelFilter = false;
         }
 
-        public IEnumerable<OpenWorkItemsResult> HungWorkItems
-        {
-            get { return _workItems; }
-        }
+        public IEnumerable<OpenWorkItemsResult> HungWorkItems { get; private set; }
 
         public IEnumerable<OpenWorkItemsResult> ImpededItems
         {
-            get { return _workItems.Where(wi => wi.HasImpediment); }
+            get { return HungWorkItems.Where(wi => wi.HasImpediment); }
         }
 
         public IEnumerable<OpenWorkItemsResult> PausedItems
         {
-            get { return _workItems.Where(wi => wi.IsPaused()); }
+            get { return HungWorkItems.Where(wi => wi.IsPaused()); }
         }
 
         public IEnumerable<OpenWorkItemsResult> StoppedItems
         {
-            get { return _workItems.Where(wi => wi.IsStopped()); }
+            get { return HungWorkItems.Where(wi => wi.IsStopped()); }
         }
 
         protected override void OnGetInternal(SqlConnection connection)
         {
             base.OnGetInternal(connection);
-            _workItems = WorkItems.Where(wi => wi.HasImpediment || wi.IsPaused() || wi.IsStopped()).ToArray();
+            HungWorkItems = WorkItems.Where(wi => wi.HasImpediment || wi.IsPaused() || wi.IsStopped()).ToArray();
         }
 
         protected override OpenWorkItems GetQuery()
