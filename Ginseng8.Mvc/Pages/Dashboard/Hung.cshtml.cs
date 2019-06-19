389bed3341703b0bbff1cf5ecbcd60322ff26db5
Adam O'Neil <adamosoftware@gmail.com>
2019-06-19 10:09:50 -0400

    Dashboard/Hung, filtered properly

--- Ginseng8.Mvc/Pages/Dashboard/Hung.cshtml.cs
+++ Ginseng8.Mvc/Pages/Dashboard/Hung.cshtml.cs
@@ -2,36 +2,55 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 
 namespace Ginseng.Mvc.Pages.Work
 {
-	[Authorize]
-	public class IndexModel : DashboardPageModel
-	{
-		public IndexModel(IConfiguration config) : base(config)
-		{
-			ShowLabelFilter = false;
-		}
+    [Authorize]
+    public class IndexModel : DashboardPageModel
+    {
+        private IEnumerable<OpenWorkItemsResult> _workItems;
 
-		public IEnumerable<OpenWorkItemsResult> ImpededItems
-		{			
-			get { return WorkItems.Where(wi => wi.HasImpediment); }
-		}
+        public IndexModel(IConfiguration config) : base(config)
+        {
+            ShowLabelFilter = false;
+        }
 
-		public IEnumerable<OpenWorkItemsResult> PausedItems
-		{
-			get { return WorkItems.Where(wi => wi.IsPaused()); }			
-		}
+        public IEnumerable<OpenWorkItemsResult> HungWorkItems
+        {
+            get { return _workItems; }
+        }
 
-		public IEnumerable<OpenWorkItemsResult> StoppedItems
-		{
-			get { return WorkItems.Where(wi => wi.IsStopped()); }			
-		}
+        public IEnumerable<OpenWorkItemsResult> ImpededItems
+        {
+            get { return _workItems.Where(wi => wi.HasImpediment); }
+        }
 
-		protected override OpenWorkItems GetQuery()
-		{
-			return new OpenWorkItems(QueryTraces) { OrgId = OrgId, AppId = CurrentOrgUser.CurrentAppId };
-		}
-	}
+        public IEnumerable<OpenWorkItemsResult> PausedItems
+        {
+            get { return _workItems.Where(wi => wi.IsPaused()); }
+        }
+
+        public IEnumerable<OpenWorkItemsResult> StoppedItems
+        {
+            get { return _workItems.Where(wi => wi.IsStopped()); }
+        }
+
+        protected override void OnGetInternal(SqlConnection connection)
+        {
+            base.OnGetInternal(connection);
+            _workItems = WorkItems.Where(wi => wi.HasImpediment || wi.IsPaused() || wi.IsStopped()).ToArray();
+        }
+
+        protected override OpenWorkItems GetQuery()
+        {
+            return new OpenWorkItems(QueryTraces)
+            {
+                OrgId = OrgId,
+                AppId = CurrentOrgUser.CurrentAppId,
+                TeamId = CurrentOrgUser.CurrentTeamId
+            };
+        }
+    }
 }
