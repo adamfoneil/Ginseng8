d14f356cb2822584138ba10791f3b626767bd0cf
adamo <adamosoftware@gmail.com>
2019-10-31 00:25:58 -0400

    moved Add Comment button to bottom of comment list

--- Ginseng8.Mvc/ViewModels/CommentView.cs
+++ Ginseng8.Mvc/ViewModels/CommentView.cs
@@ -8,6 +8,7 @@ namespace Ginseng.Mvc.ViewModels
 		public int ObjectId { get; set; }
 		public ObjectType ObjectType { get; set; }
 		public IEnumerable<Comment> Comments { get; set; }
+        public string IdPrefix { get; set; }
         public bool CommentBoxOpen { get; set; }
 	}
 }
