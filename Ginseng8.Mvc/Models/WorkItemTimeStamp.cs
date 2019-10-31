95fd7260531c70ff2c42a7067f97c017ab008a57
Adam O'Neil <adamosoftware@gmail.com>
2019-10-31 15:36:58 -0400

    showing user and date create info more visibly

---
+++ Ginseng8.Mvc/Models/WorkItemTimeStamp.cs
@@ -0,0 +1,11 @@
+﻿using Ginseng.Mvc.Queries;
+using System;
+
+namespace Ginseng.Mvc.Models
+{
+    public class WorkItemTimeStamp
+    {
+        public OpenWorkItemsResult WorkItem { get; set; }
+        public DateTime LocalTime { get; set; }
+    }
+}
