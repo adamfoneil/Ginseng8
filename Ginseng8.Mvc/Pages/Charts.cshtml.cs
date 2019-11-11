633b574bdb8dd6dcb2f45d4ab3102bc63b28ba86
Adam O'Neil <adamosoftware@gmail.com>
2019-11-11 08:58:31 -0500

    started on reports page

---
+++ Ginseng8.Mvc/Pages/Charts.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace Ginseng.Mvc.Pages
+{
+    public class ChartsModel : AppPageModel
+    {
+        public ChartsModel(IConfiguration config) : base(config)
+        {
+        }
+
+        public async Task OnGetAsync()
+        {
+
+        }
+    }
+}
