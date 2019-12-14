243b95e4f8d9805e13147c135e95257218c71940
Adam O'Neil <adamosoftware@gmail.com>
2019-12-13 22:43:37 -0500

    code cleanup

--- Ginseng8.Mvc/Startup.cs
+++ Ginseng8.Mvc/Startup.cs
@@ -16,35 +16,35 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Ginseng.Mvc
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.Configure<CookiePolicyOptions>(options =>
-			{
-				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
-				options.CheckConsentNeeded = context => true;
-				options.MinimumSameSitePolicy = SameSiteMode.None;
-			});
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
 
-			services.AddDbContext<ApplicationDbContext>(options =>
-				options.UseSqlServer(
-					Configuration.GetSection("ConnectionStrings").GetValue<string>("Default")));
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetSection("ConnectionStrings").GetValue<string>("Default")));
 
-			services
-				.AddTransient<IUserStore<IdentityUser>, ExUserStore>()
-				.AddTransient<IUserLoginStore<IdentityUser>, ExUserStore>()
-				.AddDefaultIdentity<IdentityUser>(ConfigureIdentity)
-				.AddDefaultUI(UIFramework.Bootstrap4)
-				.AddEntityFrameworkStores<ApplicationDbContext>();
+            services
+                .AddTransient<IUserStore<IdentityUser>, ExUserStore>()
+                .AddTransient<IUserLoginStore<IdentityUser>, ExUserStore>()
+                .AddDefaultIdentity<IdentityUser>(ConfigureIdentity)
+                .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddAuthentication()
                 .AddGoogle(options =>
@@ -54,7 +54,7 @@ namespace Ginseng.Mvc
                 })
                 .AddCookie()
                 .AddAerieHub(Configuration)
-				.AddGitHub(Configuration);
+                .AddGitHub(Configuration);
 
             services
                 .AddOptions()
@@ -69,41 +69,41 @@ namespace Ginseng.Mvc
                 .AddTransient<IEmailSender, Email>()
                 .AddSingleton<IViewRenderService, ViewRenderService>();
 
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-		}
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-				app.UseDatabaseErrorPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
-			app.UseCookiePolicy();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
 
-			app.UseAuthentication();
+            app.UseAuthentication();
 
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
-			});
-		}
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
 
-		private void ConfigureIdentity(IdentityOptions options)
-		{
-			// Requiring a confirmed email breaks external logins.
-			//options.SignIn.RequireConfirmedEmail = true;
-			options.User.RequireUniqueEmail = false;
-		}
-	}
+        private void ConfigureIdentity(IdentityOptions options)
+        {
+            // Requiring a confirmed email breaks external logins.
+            //options.SignIn.RequireConfirmedEmail = true;
+            options.User.RequireUniqueEmail = false;
+        }
+    }
 }
